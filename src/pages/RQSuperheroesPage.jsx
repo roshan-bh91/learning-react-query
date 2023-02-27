b8bcd5b26b0a27682bc30cbd3965672a6973b4b5
Roshan Jha <roshan@betterhalf.ai>
2023-02-27 17:24:13 +0530

    [feat]: transforming data through select

--- src/pages/RQSuperheroesPage.jsx
+++ src/pages/RQSuperheroesPage.jsx
@@ -5,12 +5,21 @@ const fetchSuperheroes = () => {
 };
 const RQSuperheroesPage = () => {
   const onSuccess = (response_details) => {
-    console.log("Side effect after successful API query",response_details);
+    console.log("Side effect after successful API query", response_details);
   };
   const onError = (error_details) => {
-    console.log("Side effect after error occurred",error_details);
+    console.log("Side effect after error occurred", error_details);
   };
 
+  // const generate_superhero_names = (response_received) => {
+  //   const superhero_names = response_received.data.map((everySuperhero) => {
+  //     return {
+  //       superhero_id: everySuperhero.superhero_id,
+  //       superhero_name: everySuperhero.superhero_name,
+  //     };
+  //   });
+  //   return superhero_names;
+  // };
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     ["superheroes"],
     fetchSuperheroes,
@@ -24,32 +33,60 @@ const RQSuperheroesPage = () => {
       enabled: false,
       onSuccess,
       onError,
+      select: (api_response) => {
+        console.log(api_response);
+        const superhero_names = api_response.data.map((everySuperhero) => {
+          return {
+            superhero_id: everySuperhero.superhero_id,
+            superhero_name: everySuperhero.superhero_name,
+          };
+        });
+        console.log(superhero_names);
+        return superhero_names;
+      },
     }
   );
-  console.log({ isLoading, isFetching });
   if (isLoading || isFetching) {
     return <h4>Loading...</h4>;
   }
   if (isError) {
     return <h4>{error.message}</h4>;
   }
+  console.log(data);
   return (
     <>
       <h4>React query superheroes page</h4>
       <button onClick={refetch}>FETCH SUPERHEROES</button>
-      {data?.data.length === 0 ? (
-        <h4>No superhero found</h4>
-      ) : (
-        <ul>
-          {data?.data.map((everySuperhero) => {
-            return (
-              <p key={everySuperhero.superhero_id}>
-                {everySuperhero.superhero_name}
-              </p>
-            );
-          })}
-        </ul>
-      )}
+      <>
+        {data?.length === 0 ? (
+          <h4>No superhero found</h4>
+        ) : (
+          <ul>
+            {data?.map((everySuperhero) => {
+              return (
+                <li key={everySuperhero.superhero_id}>
+                  {everySuperhero.superhero_name}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </>
+      <>
+        {/* {data?.data.length === 0 ? (
+          <h4>No superhero found</h4>
+        ) : (
+          <ul>
+            {data?.data.map((everySuperhero) => {
+              return (
+                <p key={everySuperhero.superhero_id}>
+                  {everySuperhero.superhero_name}
+                </p>
+              );
+            })}
+          </ul>
+        )} */}
+      </>
     </>
   );
 };
